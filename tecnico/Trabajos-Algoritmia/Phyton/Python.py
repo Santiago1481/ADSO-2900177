19f5b0f2b052a645bb8e6a5797e3b601533ffd85
Santiago Chaparro <ssprosantiago@gmail.com>
2024-06-03 21:54:18 -0500

    Actualizacion de HTML

--- tecnico/Trabajos-Algoritmia/Phyton/Python.py
+++ tecnico/Trabajos-Algoritmia/Phyton/Python.py
@@ -1,11 +1,2 @@
-# Hola MUNDO
-saludo = "Hola Mundo"
-print(saludo)
-
-
-# Suma de dos numeros 
-num1 = 7
-num2 = 8
-suma = num1 + num2
-print(num1,"+",num2,"=",suma)
-
+temps = [[0.0 for h in range(24)] for d in range(31)]
+print(temps)
